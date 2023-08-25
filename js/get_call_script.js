3a22dcedcb422ac0e049aa11924b4e00320e90c0
Rodion <rodion.chubarov@mail.ru>
2023-08-26 02:24:43 +0600

    footer + php fix

--- js/get_call_script.js
+++ js/get_call_script.js
@@ -1,7 +1,11 @@
 let popup = document.getElementById('get__call__conteiner');
 let pp_btn = document.getElementById('get__call__btn');
+let pp_btn_2 = document.getElementById('get__call__btn__2');
 let body = document.getElementsByTagName('body');
 pp_btn.onclick = function(){
+    popup.style.display="flex";
+}
+pp_btn_2.onclick = function(){
     popup.style.display="flex";
 }
 window.onclick = function (e){
--- php/teleg.php
+++ php/teleg.php
@@ -19,7 +19,7 @@ $time = $_POST['time']
 $body = '' .$time . ' '  .$city .' ' .$phone ;
 
 $url = "https://api.telegram.org/bot6062900028:AAHuWhtLDC1uFtF0PNw8GjoA_dc6an4Rfv4/sendMessage?";
-$to = "-892764569";
+$to = "-968986063";
 $text = $url."chat_id=".$to."&text=".urlencode($body);
 
 file_get_contents($text);   
